7d1c101efc4ee33b78550af01aad968ecd9f4fef
BrounieMan <juan.melendez3.14@gmail.com>
2018-02-04 22:34:39 -0600

    Fixed readability of tester/now also tests Integer arraylists

--- project1/SortTester.java
+++ project1/SortTester.java
@@ -5,8 +5,20 @@ import java.util.ArrayList;
 public class SortTester {
 
 	public static void main(String[] args) {
-		ArraySorterADT<String> sorter = new ArraySorterADT<>();
+		SorterADT<String> sorter = new ArraySorterADT<>();
+		SorterADT<Integer> sorter2 = new ArraySorterADT<>();
 		ArrayList<String> arr = new ArrayList<>();
+		ArrayList<Integer> intArr = new ArrayList<>();
+		//ArraySorterADT<Object> sorter2 = new ArraySorterADT<>(); -> bound mismatch
+		
+		intArr.add(1);
+		intArr.add(1);
+		intArr.add(2);
+		intArr.add(2);
+		intArr.add(2);
+		intArr.add(3);
+		
+		
 		
 		arr.add("Z");
 		arr.add("B5");
@@ -17,17 +29,19 @@ public class SortTester {
 		arr.add("G");
 		arr.add("A");
 		
-		System.out.println(arr);
-		System.out.println(sorter.isSorted(arr));
+		System.out.println("Expect unsorted array\t\t: "+arr);
+		System.out.println(sorter +"?\t\t: " + sorter.isSorted(arr));
 		sorter.sort(arr);
-		System.out.println(arr);
-		System.out.println(sorter.isSorted(arr));
+		System.out.println("Expect sorted array(asc)\t: "+arr);
+		System.out.println(sorter +"?\t\t: " +sorter.isSorted(arr));
 		sorter.setDescending();
-		System.out.println(sorter.isSorted(arr));
+		System.out.println(sorter +"?\t: " +sorter.isSorted(arr));
 		sorter.sort(arr);
-		System.out.println(arr);
-		System.out.println(sorter.isSorted(arr));
+		System.out.println("Expect sorted array(desc)\t: "+arr);
+		System.out.println(sorter +"?\t: " + sorter.isSorted(arr));
 		
+		System.out.println("Expect array of integers\t: " +intArr);
+		System.out.println(sorter2 +"?\t\t: " + sorter2.isSorted(intArr));
 		
 		
 	}
