1b5a3f9c646e7169b2fd23073a61ea0217d6323f
Caleb de Coux <decouxc82@gmail.com>
2018-02-06 18:05:59 -0600

    Added loader.java: creates ArrayList of WebPages using a text document.

--- project1/Loader.java
+++ project1/Loader.java
@@ -1,5 +1,45 @@
 package project1;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Loader {
+	ArrayList<WebPage> pages;
+	String filename;
+	Scanner scanner;
+	
+	
+	
+	//Methods
+	Loader(){
+		pages = new ArrayList<WebPage>();
+		filename = "";
+		
+	}
+	
+	
+	/*
+	 * Load function for an array of integer ranks. Source is defined by its position in the text.
+	 */
+	public void load(String myFile) {
+		scanner = new Scanner(myFile);
+		int i = 0;
+		int j = 0;
+		String source;
+		
+		while(scanner.hasNext()) {
+			pages.add(new WebPage());
+			
+			source = "Source" + i;
+			
+			pages.get(i).addSource();
+			
+			
+		}
+		
+		
+		scanner.close();
+	}
+	
 
 }
